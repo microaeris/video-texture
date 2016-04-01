5b0fa8d12bfbafdf5ec2e10912edcaf1b4c58383
Ahris <alicewang54@gmail.com>
2016-03-31 21:30:52 -0500

    testing out why our D is off by 2 rows/cols

--- videoTexture.m
+++ videoTexture.m
@@ -2,21 +2,24 @@
 % http://www.cc.gatech.edu/cpl/projects/videotexture/SIGGRAPH2000/videotex.pdf
 
 % OPTIONS %%%%%%%%%%%%%%%%%%%%%%%%
-INPUT_STRING = 'clock.mpg'; % Input video name
+INPUT_STRING = 'clock.mpg';       % Input video name
 OUTPUT_STRING = 'clock_loop.avi'; % Output video name
 
 % Algorithm settings
-RANDOM_PLAY = false;
-PRESERVE_MOTION = false;
+RANDOM_PLAY = true;
+PRESERVE_MOTION = true;
 
 % Parameters
-NEIGHBORS = 2;
+NEIGHBORS = 1;
+
+colormap gray
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Find number of frames
 videoStream = VideoReader(INPUT_STRING);
 frameRate = videoStream.FrameRate;
 numFrames = 0;
+
 while hasFrame(videoStream)
   readFrame(videoStream);
   numFrames = numFrames + 1;
@@ -33,17 +36,16 @@ D_s = zeros(1, lenIndex);
 
 i = 1;
 k = 1;
+
 while hasFrame(videoStream1)
   frame1 = double(readFrame(videoStream1));
   videoStream2 = VideoReader(INPUT_STRING);
-  
   j = 1;
+  
   while hasFrame(videoStream2)
     frame2 = double(readFrame(videoStream2));
-    %     dist = sqrt(sum((frame1(:) - frame2(:)).^ 2));
     dist = sum((frame1(:) - frame2(:)).^2);
     
-    %     curIndex = (j-1) * numFrames + i;
     D_i(k) = i;
     D_j(k) = j;
     D_s(k) = dist;
@@ -58,16 +60,24 @@ end
 % Distance matrix between frames
 D = sparse(D_i, D_j, D_s);
 
+imagesc(full(D));
+% imagesc(full(circshift(D, [-1 0])));
+
 if PRESERVE_MOTION
   % Weight neighboring frames to preserve continuous motion
-  W = zeros(1, NEIGHBORS * 2);
+  W_len = NEIGHBORS * 2;
+  W = zeros(1, W_len);
   
-  for i = 1:length(W)
-    W(i) = normpdf(i, NEIGHBORS, .5);
+  % Binomial coefficient weights
+  for i = 1:W_len
+    W(i) = nchoosek(W_len, i - 1);
   end
-
+  
   W = diag(W);
   D = sparse(imfilter(full(D), W));
+    
+  [h, w] = size(D); 
+  D = D(NEIGHBORS:h - NEIGHBORS, NEIGHBORS:w - NEIGHBORS);
 end
 
 % Smaller values of sigma emphasize just the very best
@@ -80,9 +90,10 @@ P = exp(-1 * circshift(D, [0 -1]) / sigma);
 
 % Normalize probabilities across i so sum of Pij = 1 for all j
 sumRows = sum(P, 2);
-P = P ./ repmat(sumRows, 1, numFrames);
+P = P ./ repmat(sumRows, 1, size(P, 2));
 
-imagesc(full(D));
+% imshow(full(D), [0 max(max(D))]);
+% imagesc(full(D));
 % imagesc(full(P));
 
 if RANDOM_PLAY
